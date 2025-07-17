700ad7660cba8e2fb77257f0f983f28e0c19a1bb
no-okane <dnjsgml3304.15@gmail.com>
2025-07-17 18:07:19 +0900

    2025-07-17

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -8,26 +8,34 @@ const follwUs = new Swiper ('#insta',{
     //loopAdditionalSlides: 6,
 })
 
+const newProduct = new Swiper ('#new_product',{
+    loop:true,
+    scrollbar:{
+        el:'#new_product + .swiper-controls .swiper-scrollbar'
+    },
+    pagination: {
+        el: "#new_product + .swiper-controls .pagination_deco .swiper-pagination",
+        type: "fraction",
+        clickable: true,
+    },
+})
+
+
 const adBnr = new Swiper ('#ad_bnr',{
     loop:true,
-    //spaceBetween:0,
-    //slidesPerView : 0,
     autoplay:{
         delay:3500,
         disableOnInteraction: false,
     },
-    //slidesPerGroup: 1,  // 슬라이드를 그룹화하여 한 번에 1개씩 이동
-    //speed:0,
-    //loopAdditionalSlides: 6,
     navigation:{
         nextEl:'#ad_bnr .swiper-button-next',
         prevEl:'#ad_bnr .swiper-button-prev',
     },
     scrollbar:{
-        el:'.swiper-controls .swiper-scrollbar'
+        el:'#ad_bnr + .swiper-controls .swiper-scrollbar'
     },
     pagination: {
-        el: ".swiper-controls .swiper-pagination",
+        el: "#ad_bnr + .swiper-controls .pagination_deco .swiper-pagination",
         type: "fraction",
         clickable: true,
     },
